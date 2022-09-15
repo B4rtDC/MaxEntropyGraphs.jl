846397ce53a8e7e811c3b0333acc84ca8d66cbf3
Bart De Clerck <declerck.bart@pm.me>
2022-09-15 11:21:56 +0200

    updated

--- testscript.jl
+++ testscript.jl
@@ -1,7 +1,6 @@
-using Graphs
+
 using MaxEntropyGraphs
 
-g = erdos_renyi(50,0.1)
-model = UBCM(g)
 
-mymodel = DBCM(erdos_renyi(50,0.1, is_directed=true))
+
+MaxEntropyGraphs.produce_squartini_dbcm_data("./data/computed_results/DBCM_result_NS22.jld", "./data/networks")
