8c094b415a2d1b08cd4d2fd77dfc7997de2775e7
Bart De Clerck <declerck.bart@pm.me>
2023-07-19 18:08:15 +0200

    added tests for optmiisation

--- examples/DBCM_demo.jl
+++ examples/DBCM_demo.jl
@@ -4,6 +4,7 @@
 # This file contains some demos for the DBCM model
 ##################################################################################
 
+# setup for the DBCM model
 begin
     using Revise
     using BenchmarkTools
@@ -108,34 +109,8 @@ begin
         0.000000000000000000000000000000000000000000000000000000000000
         0.000000000000000000000000000000000000000000000000000000000000
     ]
-    const DBCM_pysol_iter = [ # these are the alpha/beta values
-        1.981160141317101608393613787484355270862579345703125000000000
-        1.959941522570228578103979089064523577690124511718750000000000
-        1.935725469173092960772919468581676483154296875000000000000000
-        1.232759259644305993575130742101464420557022094726562500000000
-        1.189022937587318695662474965502042323350906372070312500000000
-        1.137073941778148800096914783352985978126525878906250000000000
-        0.725859771090746064281518101779511198401451110839843750000000
-        0.676064144843745884472241414187010377645492553710937500000000
-        0.378570991970373749158085274757468141615390777587890625000000
-        0.352551660693394663947941580772749148309230804443359375000000
-        0.301374016447932324691549865747219882905483245849609375000000
-        0.097186852896756686925883172989415470510721206665039062500000
-        -0.013834421741768944402739549559555598534643650054931640625000
-        1.388761003744900301981601842271629720926284790039062500000000
-        0.905502169119994038126719715364743024110794067382812500000000
-        0.535350182042531819170960716292029246687889099121093750000000
-        2.136921769368255397125722083728760480880737304687500000000000
-        0.882204908579032887061543988238554447889328002929687500000000
-        0.195609393060857422730336452332267072051763534545898437500000
-        1.345022275861593952228645321156363934278488159179687500000000
-        0.483530796363261350379048053582664579153060913085937500000000
-        2.091426793023562158424510926124639809131622314453125000000000
-        1.320035452208659254580425113090313971042633056640625000000000
-        0.458258322810216167653152297134511172771453857421875000000000
-        Inf
-        0.433991454737065240898630236188182607293128967285156250000000
-    ]
+    const DBCM_pysol_iter = [2.045354770817971790108913410222157835960388183593750000000000,2.024136152071098315730068861739709973335266113281250000000000,1.999920098673962920443614166288170963525772094726562500000000,1.296953889145175509156615589745342731475830078125000000000000,1.253217567088188655333169663208536803722381591796875000000000,1.201268571279018537723004556028172373771667480468750000000000,0.790054400591615690885305411939043551683425903320312500000000,0.740258774344615844142936111893504858016967773437500000000000,0.442765621471243597806477509948308579623699188232421875000000,0.416746290194264734640938740994897671043872833251953125000000,0.365568645948802117828790869680233299732208251953125000000000,0.161381482397626285774094867520034313201904296875000000000000,0.050360207759100855673395358280686195939779281616210937500000,
+                            1.324566374244030786400116994627751410007476806640625000000000,0.841307539619124522545234867720864713191986083984375000000000,0.471155552541661970522568481101188808679580688476562500000000,2.072727139867386103588842161116190254688262939453125000000000,0.818010279078163149435454215563368052244186401367187500000000,0.131414763559987435304066138996859081089496612548828125000000,1.280827646360724436647160473512485623359680175781250000000000,0.419336166862391335197202124618343077600002288818359375000000,2.027232163522692864887631003512069582939147949218750000000000,1.255840822707789961043545190477743744850158691406250000000000,0.394063693309345985937852674396708607673645019531250000000000,Inf,0.369796825236195114694481844708207063376903533935546875000000]
     const DBCM_newton_time = 0.12884306907653809
     const DBCM_quasinewton_time = 0.0054857730865478516
     const DBCM_fixedpoint_time = 0.0038568973541259766
@@ -210,7 +185,7 @@ end
 begin
     model = MaxEntropyGraphs.DBCM(d_out=Int32.(d_out_py), d_in=Int32.(d_in_py))
     # setting the solution parameters
-    θ = vcat(DBCM_pysol_alpha, DBCM_pysol_beta) 
+    θ = DBCM_pysol_iter#vcat(DBCM_pysol_alpha, DBCM_pysol_beta) 
     model.θᵣ .= θ
     model.xᵣ .= exp.(-DBCM_pysol_alpha)
     model.yᵣ .= exp.(-DBCM_pysol_beta)
@@ -219,15 +194,14 @@ begin
     #θbuffer = copy(model.θᵣ)
     x = similar(model.xᵣ)
     y = similar(model.yᵣ)
-    G = similar(model.xᵣ)
-    H = similar(model.yᵣ)
+    G = similar(model.θᵣ) 
+    #H = similar(model.yᵣ)
     
-    res = MaxEntropyGraphs.DBCM_reduced_iter!(θ, model.dᵣ_out, model.dᵣ_in, model.f, model.dᵣ_out_nz, model.dᵣ_in_nz, x, y, G, H, model.status[:d_unique])
-
+    res = MaxEntropyGraphs.DBCM_reduced_iter!(θ, model.dᵣ_out, model.dᵣ_in, model.f, model.dᵣ_out_nz, model.dᵣ_in_nz, x, y, G, model.status[:d_unique])
+    res[isinf.(θ)] .= Inf
     @assert res ≈ DBCM_pysol_iter
-    @btime MaxEntropyGraphs.DBCM_reduced_iter!(θ, model.dᵣ_out, model.dᵣ_in, model.f, model.dᵣ_out_nz, model.dᵣ_in_nz, x, y, G, H, model.status[:d_unique]);
+    @btime MaxEntropyGraphs.DBCM_reduced_iter!(θ, model.dᵣ_out, model.dᵣ_in, model.f, model.dᵣ_out_nz, model.dᵣ_in_nz, x, y, G, model.status[:d_unique]);
     nothing
-
 end
 
 # testing the gradient function
@@ -255,9 +229,131 @@ begin
     nothing
 end
 
+# using a gradient based method (LBDFGS) with Optim.jl using Zygote for automatic differentiation (for small scale problems, i.e. less than 100 parameters, you should you ForwardDiff.jl instead of Zygote.jl)
+# _______________________________________________________________________________________________
+begin
+    using Zygote
+    # make the model
+    G = MaxEntropyGraphs.Graphs.SimpleDiGraphFromIterator(MaxEntropyGraphs.Graphs.Edge.(edgelist_jul))
+    model = DBCM(G);
+    # set the initial guess
+    θ₀ = initial_guess(model);
+    # define the objective function
+    obj = (θ, p) -> - MaxEntropyGraphs.L_DBCM_reduced(θ, model.dᵣ_out, model.dᵣ_in, model.f, model.dᵣ_out_nz, model.dᵣ_in_nz);
+    f = MaxEntropyGraphs.Optimization.OptimizationFunction(obj, MaxEntropyGraphs.Optimization.AutoForwardDiff());
+    # defin the optimisation problem
+    prob = MaxEntropyGraphs.Optimization.OptimizationProblem(f, θ₀);
+    # actual optimisation (gradient free)
+    sol = MaxEntropyGraphs.Optimization.solve(prob, MaxEntropyGraphs.OptimizationOptimJL.LBFGS());
+    @info "Likelihood value: $(obj(sol.u, nothing))"
+    @info """Difference in likelihood with NEMtropy: $(MaxEntropyGraphs.@sprintf("%1.2e",-obj(sol.u, nothing) - DBCM_pysol_L)) (i.e. we get a slightly better likelihood)"""
+    nanind = .!isinf.(sol.u)
+    @info """Largest difference in parameter value: $(MaxEntropyGraphs.@sprintf("%1.2e",maximum(abs.(sol.u[nanind] - vcat(DBCM_pysol_alpha, DBCM_pysol_beta)[nanind]))))"""
+    # store solution and compute adjacency matrix
+    model.θᵣ .= sol.u
+    model.status[:params_computed] = true;
+    set_xᵣ!(model) 
+    set_yᵣ!(model)
+    set_Ĝ!(model) 
+    @info "Maximum outdegree error for LBFGS iteration (autodiff): $(MaxEntropyGraphs.@sprintf("%.2e", maximum(abs.(sum(Ĝ(model), dims=2) .- model.dᵣ_out[model.dᵣ_ind]))))"
+    @info "Maximum indegree error for LBFGS iteration (autodiff):  $(MaxEntropyGraphs.@sprintf("%.2e", maximum(abs.(reshape(sum(Ĝ(model), dims=1),:,1) .- model.dᵣ_in[model.dᵣ_ind]))))"
+    DBCM_autodiff_perf = @benchmark MaxEntropyGraphs.Optimization.solve($(prob), $(MaxEntropyGraphs.OptimizationOptimJL.LBFGS()));
+    @info "Median compute time for for LBFGS iteration (autodiff): $(MaxEntropyGraphs.@sprintf("%2.2e", median(DBCM_autodiff_perf).time/1e9)) - speedup vs. NEMtropy (quasinewton): x$(MaxEntropyGraphs.@sprintf("%1.2f", DBCM_quasinewton_time/(median(DBCM_autodiff_perf).time/1e9))))";
+end 
+
+# using a gradient based method (LBDFGS) with Optim.jl using own gradient
+ begin
+    # make the model
+    G = MaxEntropyGraphs.Graphs.SimpleDiGraphFromIterator(MaxEntropyGraphs.Graphs.Edge.(edgelist_jul))
+    model = DBCM(G);
+    # set the initial guess
+    θ₀ = initial_guess(model);
+    # define the objective function
+    obj = (θ, p) -> - MaxEntropyGraphs.L_DBCM_reduced(θ, model.dᵣ_out, model.dᵣ_in, model.f, model.dᵣ_out_nz, model.dᵣ_in_nz);
+    # define the gradient and the required buffers
+    G = similar(θ₀)
+    x = Vector{eltype(θ₀)}(undef, length(model.xᵣ))
+    y = Vector{eltype(θ₀)}(undef, length(model.yᵣ))
+    grad! = (G, θ, p) -> MaxEntropyGraphs.∇L_DBCM_reduced_minus!(G, θ, model.dᵣ_out, model.dᵣ_in, model.f, model.dᵣ_out_nz, model.dᵣ_in_nz, x, y, model.status[:d_unique]);
+    f = MaxEntropyGraphs.Optimization.OptimizationFunction(obj, grad=grad!);
+    # defin the optimisation problem
+    prob = MaxEntropyGraphs.Optimization.OptimizationProblem(f, θ₀);
+    # actual optimisation (gradient free)
+    sol = MaxEntropyGraphs.Optimization.solve(prob, MaxEntropyGraphs.OptimizationOptimJL.LBFGS());
+    # obtained likelihood
+    @info "Likelihood value: $(obj(sol.u, nothing))"
+    @info """Difference in likelihood with NEMtropy: $(MaxEntropyGraphs.@sprintf("%1.2e",-obj(sol.u, nothing) - DBCM_pysol_L)) (i.e. we get a slightly better likelihood)"""
+    nanind = .!isinf.(sol.u)
+    @info """Largest difference in parameter value: $(MaxEntropyGraphs.@sprintf("%1.2e",maximum(abs.(sol.u[nanind] - vcat(DBCM_pysol_alpha, DBCM_pysol_beta)[nanind]))))"""
+    # store solution and compute adjacency matrix
+    model.θᵣ .= sol.u
+    model.status[:params_computed] = true;
+    set_xᵣ!(model) 
+    set_yᵣ!(model)
+    set_Ĝ!(model) 
+    @info "Maximum outdegree error for LBFGS iteration: $(MaxEntropyGraphs.@sprintf("%.2e", maximum(abs.(sum(Ĝ(model), dims=2) .- model.dᵣ_out[model.dᵣ_ind]))))"
+    @info "Maximum indegree error for LBFGS iteration:  $(MaxEntropyGraphs.@sprintf("%.2e", maximum(abs.(reshape(sum(Ĝ(model), dims=1),:,1) .- model.dᵣ_in[model.dᵣ_ind]))))"
+    DBCM_own_grad_perf = @benchmark MaxEntropyGraphs.Optimization.solve($(prob), $(MaxEntropyGraphs.OptimizationOptimJL.LBFGS()));
+    @info "Median compute time for for LBFGS iteration: $(MaxEntropyGraphs.@sprintf("%2.2e", median(DBCM_own_grad_perf).time/1e9)) - speedup vs. NEMtropy (quasinewton): x$(MaxEntropyGraphs.@sprintf("%1.2f", DBCM_quasinewton_time/(median(DBCM_own_grad_perf).time/1e9))))";
+end
 
 
+# using a fixed point approach leveraging NLsolve.jl through anderson acceleration
+# ________________________________________________________________________________
+begin
+    using NLsolve
+    # make the model
+    G = MaxEntropyGraphs.Graphs.SimpleDiGraphFromIterator(MaxEntropyGraphs.Graphs.Edge.(edgelist_jul))
+    model = DBCM(G);
+    # define the objective function
+    obj = (θ, p) -> - MaxEntropyGraphs.L_DBCM_reduced(θ, model.dᵣ_out, model.dᵣ_in, model.f, model.dᵣ_out_nz, model.dᵣ_in_nz);
+    # set the initial guess
+    θ₀ = initial_guess(model);
+    x_buffer = zeros(length(model.dᵣ_out)); # buffer for x = exp(-θ)
+    y_buffer = zeros(length(model.dᵣ_in));  # buffer for y = exp(-θ)
+    G_buffer = zeros(length(model.θᵣ)); # buffer for G(x)
+    #H_buffer = zeros(length(model.dᵣ_in));  # buffer for H(y)
+    DBCM_FP! = (θ::Vector) -> MaxEntropyGraphs.DBCM_reduced_iter!(θ, model.dᵣ_out, model.dᵣ_in, model.f, model.dᵣ_out_nz, model.dᵣ_in_nz, x_buffer, y_buffer, G_buffer, model.status[:d_unique])
+    #DBCM_FP! = (θ::Vector) -> MaxEntropyGraphs.DBCM_reduced_iter!(θ, model.dᵣ_out, model.dᵣ_in, model.f, model.dᵣ_out_nz, model.dᵣ_in_nz, x_buffer, y_buffer, G_buffer, H_buffer, model.status[:d_unique])
+    # set the initial guess
+    θ₀ = initial_guess(model);
+    # avoid Inf values for compute
+    ind_inf = θ₀ .== Inf
+    θ₀[ind_inf] .= 1e8
+    # get a solution
+    sol = fixedpoint(DBCM_FP!, θ₀, method=:anderson, ftol=1e-12, iterations=1000);
+    model.θᵣ .= sol.zero;
+    model.θᵣ[ind_inf] .= Inf # rectify to Inf
+    model.status[:params_computed] = true;
+    set_xᵣ!(model) 
+    set_yᵣ!(model)
+    set_Ĝ!(model) 
+    # obtained likelihood
+    @info "Likelihood value: $(obj(sol.zero, nothing))"
+    @info """Difference in likelihood with NEMtropy: $(MaxEntropyGraphs.@sprintf("%1.2e",-obj(model.θᵣ, nothing) - DBCM_pysol_L)) (i.e. slightly smaller value)"""
+    nanind = .!isinf.(model.θᵣ)
+    @info """Largest difference in parameter value: $(MaxEntropyGraphs.@sprintf("%1.2e",maximum(abs.(sol.zero[nanind] - DBCM_pysol_iter[nanind]))))"""
+    @info "Maximum outdegree error for LBFGS iteration: $(MaxEntropyGraphs.@sprintf("%.2e", maximum(abs.(sum(Ĝ(model), dims=2) .- model.dᵣ_out[model.dᵣ_ind]))))"
+    @info "Maximum indegree error for LBFGS iteration:  $(MaxEntropyGraphs.@sprintf("%.2e", maximum(abs.(reshape(sum(Ĝ(model), dims=1),:,1) .- model.dᵣ_in[model.dᵣ_ind]))))"
+    DBCM_FP_perf = @benchmark fixedpoint($(DBCM_FP!), $(θ₀), method=:anderson, ftol=1e-12, iterations=1000);
+    @info "Median compute time for FP iteration:  $(MaxEntropyGraphs.@sprintf("%2.2es", median(DBCM_FP_perf).time/1e9)) - speedup vs. NEMtropy (fixedpoint): x$(MaxEntropyGraphs.@sprintf("%1.2f", DBCM_fixedpoint_time/(median(DBCM_FP_perf).time/1e9))))";
+end
 
+
+# work with a random graph
+# ________________________
+begin
+    G = MaxEntropyGraphs.Graphs.SimpleGraphs.barabasi_albert(1000,3, is_directed=true)
+    model = DBCM(G);
+    solve_model!(model)
+    model.θᵣ
+    A = MaxEntropyGraphs.set_Ĝ!(model);
+    outdegree(G)
+    indegree(G)
+    maximum(abs.(sum(A, dims=2) - outdegree(G))), maximum(abs.(reshape(sum(A, dims=1),:,1) - indegree(G)))
+    maximum(mean(hcat(map(outdegree, rand(model, 1000))...), dims=2) - outdegree(G)), minimum(mean(hcat(map(outdegree, rand(model, 1000))...), dims=2) - outdegree(G)), mean(mean(hcat(map(outdegree, rand(model, 1000))...), dims=2) - outdegree(G))
+    #reshape(sum(A, dims=1),:,1) - indegree(G)
+end
 
 
 
