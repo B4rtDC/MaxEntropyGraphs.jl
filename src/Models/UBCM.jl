f221f55c169f419a684ab520efcd8fef93c7cf08
Bart De Clerck <declerck.bart@pm.me>
2023-09-04 18:22:24 +0200

    docs

--- src/Models/UBCM.jl
+++ src/Models/UBCM.jl
@@ -246,20 +246,20 @@ julia> ∇model_fun!(model.Θᵣ);
 julia> model = UBCM(MaxEntropyGraphs.Graphs.SimpleGraphs.smallgraph(:karate));
 
 julia> fun =   (θ, p) ->  - L_UBCM_reduced(θ, model.dᵣ, model.f);
-# initialise gradient buffer
-julia> x  = zeros(Real, length(model.Θᵣ));
-# define gradient
-julia> ∇fun! = (∇L, θ, p) -> ∇L_UBCM_reduced!(∇L, θ, model.dᵣ, model.f, x);
-# initial condition
-julia> θ₀ = initial_guess(model); 
-# define target function 
-julia> foo = MaxEntropyGraphs.Optimization.OptimizationProblem(fun, grad=∇fun!);
-# define the optimisation problem
-julia> prob  = MaxEntropyGraphs.Optimization.OptimizationFunction(prob, θ₀);
-# set the optimisation method
-julia> method = MaxEntropyGraphs.OptimizationOptimJL.LBFGS();
-# solve it
-julia> MaxEntropyGraphs.Optimization.solve(prob, method);
+
+julia> x  = zeros(Real, length(model.Θᵣ)); # initialise gradient buffer
+
+julia> ∇fun! = (∇L, θ, p) -> ∇L_UBCM_reduced!(∇L, θ, model.dᵣ, model.f, x); # define gradient
+
+julia> θ₀ = initial_guess(model); # initial condition
+
+julia> foo = MaxEntropyGraphs.Optimization.OptimizationProblem(fun, grad=∇fun!); # define target function 
+
+julia> prob  = MaxEntropyGraphs.Optimization.OptimizationFunction(prob, θ₀); # define the optimisation problem
+
+julia> method = MaxEntropyGraphs.OptimizationOptimJL.LBFGS(); # set the optimisation method
+
+julia> MaxEntropyGraphs.Optimization.solve(prob, method); # solve it
 
 ```
 """
