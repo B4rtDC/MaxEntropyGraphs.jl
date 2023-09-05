e803e54c8e3d472ba2f15107615e2c6808884b34
Bart De Clerck <declerck.bart@pm.me>
2023-09-05 20:54:54 +0200

    more docs

--- src/Models/UBCM.jl
+++ src/Models/UBCM.jl
@@ -716,4 +716,25 @@ function solve_model!(m::UBCM{T,N};  # common settings
     return m, sol
 end
 
+
+"""
+    precision(m::UBCM)
+
+Determine the compute precision of the UBCM model `m`.
+
+# Examples
+```jldoctest
+julia> model = UBCM(MaxEntropyGraphs.Graphs.SimpleGraphs.smallgraph(:karate));
+
+julia> MaxEntropyGraphs.precision(model)
+Float64
+```
+
+```jldoctest
+julia> model = UBCM(MaxEntropyGraphs.Graphs.SimpleGraphs.smallgraph(:karate), precision=Float32);
+
+julia> MaxEntropyGraphs.precision(model)
+Float32
+```
+"""
 precision(m::UBCM) = typeof(m).parameters[2]
