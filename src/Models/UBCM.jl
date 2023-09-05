84639ebb136132ed02aef84be818446eb87860ce
Bart De Clerck <declerck.bart@pm.me>
2023-09-05 19:52:20 +0200

    docs

--- src/Models/UBCM.jl
+++ src/Models/UBCM.jl
@@ -405,15 +405,23 @@ end
 Compute an initial guess for the maximum likelihood parameters of the UBCM model `m` using the method `method`.
 
 The methods available are: 
-- `:degrees` (default): the initial guess is computed using the degrees of the graph: ``\\theta_{i} = -\\log(d_{i})`` 
-- `:degrees_minor`: the initial guess is computed using the degrees of the graph and the number of edges: ``\\theta_{i} = -\\log(d_{i}/(\\sqrt{E} + 1))``
-- `:random`, 
-- `:uniform`, 
-- `:chung_lu`.
+- `:degrees` (default): the initial guess is computed using the degrees of the graph, i.e. ``\\theta_{i} = -\\log(d_{i})`` 
+- `:degrees_minor`: the initial guess is computed using the degrees of the graph and the number of edges, i.e. ``\\theta_{i} = -\\log(d_{i}/(\\sqrt{E} + 1))``
+- `:random`: the initial guess is computed using random values between 0 and 1, i.e. ``\\theta_{i} = -\\log(r_{i})`` where ``r_{i} \\sim U(0,1)``
+- `:uniform`: the initial guess is uniformily set to 0.5, i.e. ``\\theta_{i} = -\\log(0.5)``
+- `:chung_lu`: the initial guess is computed using the degrees of the graph and the number of edges, i.e. ``\\theta_{i} = -\\log(d_{i}/(2E))``
 
 # Examples
 ```jldoctest
 julia> model = UBCM(MaxEntropyGraphs.Graphs.SimpleGraphs.smallgraph(:karate));
+
+julia> initial_guess(model, method=:random);
+
+julia> initial_guess(model, method=:uniform);
+
+julia> initial_guess(model, method=:degrees_minor);
+
+julia> initial_guess(model, method=:chung_lu);
 
 julia> initial_guess(model)
 11-element Vector{Float64}:
@@ -428,13 +436,6 @@ julia> initial_guess(model)
  -2.4849066497880004
  -2.772588722239781
  -2.833213344056216
- 
-```
-
-```jldoctest
-julia> initial_guess(model, method=:random);
-
-julia> initial_guess(model, method=:uniform);
 
 ```
 """
