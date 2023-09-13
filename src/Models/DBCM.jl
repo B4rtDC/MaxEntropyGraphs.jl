2c518a9488b77c82dc4cfb4613286699aebc490c
Bart De Clerck <declerck.bart@pm.me>
2023-09-13 18:40:38 +0200

    added degree (similar Graphs.degree)

--- src/Models/DBCM.jl
+++ src/Models/DBCM.jl
@@ -1079,6 +1079,62 @@ Vector{Float64} (alias for Array{Float64, 1})
 """
 indegree(m::DBCM, v::Vector{Int}=collect(1:m.status[:d]); method::Symbol=:reduced) = [indegree(m, i, method=method) for i in v]
 
+"""
+    degree(m::DBCM, i::Int; method=:reduced)
+
+In alignment with `Graphs.jl`, returns the sum of the expected out- and indegree for node `i` of the DBCM model `m`.
+Uses the reduced model parameters `xᵣ` for perfomance reasons.
+
+# Arguments
+- `m::DBCM`: the DBCM model
+- `i::Int`: the node for which to compute the degree.
+- `method::Symbol`: the method to use for computing the degree. Can be any of the following:
+    - `:reduced` (default) uses the reduced model parameters `xᵣ` for perfomance reasons.
+    - `:full` uses all elements of the expected adjacency matrix.
+    - `:adjacency` uses the precomputed adjacency matrix `m.Ĝ` of the model.
+
+# Examples
+```jldoctest
+julia> model = DBCM(MaxEntropyGraphs.Graphs.SimpleDiGraph(rhesus_macaques()));
+
+julia> solve_model!(model);
+
+julia> set_Ĝ!(model);
+
+julia> typeof([degree(model, 1), degree(model, 1, method=:full), degree(model, 1, method=:adjacency)])
+Vector{Float64} (alias for Array{Float64, 1})
+
+``` 
+"""
+degree(m::DBCM, i::Int; method::Symbol=:reduced) = outdegree(m, i, method=method) + indegree(m, i, method=method)
+
+"""
+    degree(m::DBCM[, v]; method=:reduced)
+
+In alignment with `Graphs.jl`, returns a vector corresponding to the sum of the expected out- and indegree of the DBCM model `m` each node. If v is specified, only return indegrees for nodes in v.
+
+# Arguments
+- `m::DBCM`: the DBCM model
+- `v::Vector{Int}`: the nodes for which to compute the degree. Default is all nodes.
+- `method::Symbol`: the method to use for computing the degree. Can be any of the following:
+    - `:reduced` (default) uses the reduced model parameters `xᵣ` for perfomance reasons.
+    - `:full` uses all elements of the expected adjacency matrix.
+    - `:adjacency` uses the precomputed adjacency matrix `m.Ĝ` of the model.
+
+# Examples
+```jldoctest
+julia> model = DBCM(MaxEntropyGraphs.Graphs.SimpleDiGraph(rhesus_macaques()));
+
+julia> solve_model!(model);
+
+julia> set_Ĝ!(model);
+
+julia> typeof(degree(model, method=:adjacency)) 
+Vector{Float64} (alias for Array{Float64, 1})
+
+``` 
+"""
+degree(m::DBCM, v::Vector{Int}=collect(1:m.status[:d]); method::Symbol=:reduced) = outdegree(m, v, method=method) + indegree(m, v, method=method)
 
 """
     AIC(m::DBCM)
